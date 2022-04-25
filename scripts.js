25055b1c14b10a2c12a41cbe71e5b523a7f0af30
Kyler <kylerjung121@gmail.com>
2022-04-25 19:12:35 -0400

    added a ton of stuff, media queries, some javascript for the nav, and extra stylings

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".mobileNav")
+const ul = document.querySelector(".desktopNav")
+const a = document.querySelectorAll(".links")
+
+
+menu.addEventListener("click", function() {
+  ul.classList.toggle("click")
+});
+
+a.forEach(link => {
+  link.addEventListener("click", function handleClick(event) {
+    ul.classList.toggle("click")
+  })
+})
